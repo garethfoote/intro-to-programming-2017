ab53efbfd1f6e47f1d876fc0a005e3e226ed0201
garethfoote <gareth.foote@gmail.com>
2017-11-13 10:10:13 +0000

    Frame rate on screen

--- 00_empty_project/sketch.js
+++ 00_empty_project/sketch.js
@@ -2,10 +2,14 @@
  * An empty p5.js project
  * @author Write your name here.
  */
+var count = 0;
+var date = new Date()
+
 function setup() {
-  console.log( "Setup" );
+  console.log( "Hello World", date.toDateString() );
 }
 
 function draw() {
-  console.log( "Draw" );
+  background(200);
+  text(frameCount, 0, 10);
 }
